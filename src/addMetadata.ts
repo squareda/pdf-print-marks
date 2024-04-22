0ad076f53558a98c5036527651cd5296df04daec
Alex Brazier <aejbrazier.apps@gmail.com>
2024-04-22 13:35:54 +0100

    Outline metadata text

--- src/addMetadata.ts
+++ src/addMetadata.ts
@@ -1,6 +1,35 @@
-import { PDFPage } from "@cantoo/pdf-lib";
+import {
+  PDFPage,
+  rgb,
+  setFillingColor,
+  setStrokingColor,
+  setLineWidth,
+  popGraphicsState,
+  pushGraphicsState,
+  setTextRenderingMode,
+  TextRenderingMode,
+  PDFPageDrawTextOptions,
+} from "@cantoo/pdf-lib";
 
 const padDate = (date: number) => date.toString().padStart(2, "0");
+
+const drawOutlinedText = (
+  page: PDFPage,
+  text: string,
+  options: PDFPageDrawTextOptions
+) => {
+  page.pushOperators(
+    pushGraphicsState(),
+    setFillingColor(rgb(0, 0, 0)),
+    setStrokingColor(rgb(1, 1, 1)),
+    setTextRenderingMode(TextRenderingMode.FillAndOutline),
+    setLineWidth((options.size || 25) / 25)
+  );
+
+  page.drawText(text, options);
+
+  page.pushOperators(popGraphicsState());
+};
 
 const addMetadata = (
   page: PDFPage,
@@ -9,7 +38,7 @@ const addMetadata = (
   pageNumber: number,
   bleed = 0
 ) => {
-  page.drawText(`${name}   ${pageNumber}`, {
+  drawOutlinedText(page, `${name}   ${pageNumber}`, {
     x: 24 + bleed,
     y: 10 + bleed,
     size: 6,
@@ -20,7 +49,7 @@ const addMetadata = (
   )}/${date.getFullYear()}   ${padDate(date.getHours())}:${padDate(
     date.getMinutes()
   )}`;
-  page.drawText(dateString, {
+  drawOutlinedText(page, dateString, {
     x: page.getWidth() - 74 - bleed,
     y: 10 + bleed,
     size: 6,
