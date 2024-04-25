301701eb22b2ed0bf1d72f4b8dc23baef8226b5a
Alex Brazier <aejbrazier.apps@gmail.com>
2024-04-25 07:59:25 +0100

    Update pdf-lib to use new text stroke option

--- src/addMetadata.ts
+++ src/addMetadata.ts
@@ -1,35 +1,11 @@
 import {
   PDFPage,
   rgb,
-  setFillingColor,
-  setStrokingColor,
-  setLineWidth,
-  popGraphicsState,
-  pushGraphicsState,
-  setTextRenderingMode,
   TextRenderingMode,
   PDFPageDrawTextOptions,
 } from "@cantoo/pdf-lib";
 
 const padDate = (date: number) => date.toString().padStart(2, "0");
-
-const drawOutlinedText = (
-  page: PDFPage,
-  text: string,
-  options: PDFPageDrawTextOptions
-) => {
-  page.pushOperators(
-    pushGraphicsState(),
-    setFillingColor(rgb(0, 0, 0)),
-    setStrokingColor(rgb(1, 1, 1)),
-    setTextRenderingMode(TextRenderingMode.FillAndOutline),
-    setLineWidth((options.size || 20) / 40)
-  );
-
-  page.drawText(text, options);
-
-  page.pushOperators(popGraphicsState());
-};
 
 const SPACING_X = 24;
 const SPACING_Y = 10;
@@ -41,12 +17,18 @@ const addMetadata = (
   pageNumber: number,
   bleed = 0
 ) => {
-  const size = 6;
-  drawOutlinedText(page, `${name}   ${pageNumber}`, {
+  const outlinedText: Partial<PDFPageDrawTextOptions> = {
+    strokeWidth: 0.15,
+    strokeColor: rgb(1, 1, 1),
+    renderMode: TextRenderingMode.FillAndOutline,
+    size: 6,
+  };
+  page.drawText(`${name}   ${pageNumber}`, {
     x: SPACING_X + bleed,
     y: 10 + bleed,
-    size,
+    ...outlinedText,
   });
+
   // Format dd/mm/yyyy   HH:MM
   const dateString = `${padDate(date.getDate())}/${padDate(
     date.getMonth() + 1
@@ -54,11 +36,11 @@ const addMetadata = (
     date.getMinutes()
   )}`;
   const font = page.getFont()[0];
-  const textWidth = font.widthOfTextAtSize(dateString, size);
-  drawOutlinedText(page, dateString, {
+  const textWidth = font.widthOfTextAtSize(dateString, outlinedText.size!);
+  page.drawText(dateString, {
     x: page.getWidth() - SPACING_X - textWidth - bleed,
     y: SPACING_Y + bleed,
-    size,
+    ...outlinedText,
   });
 };
 
